c5319d38d0b28883aa0763861fef4261b957b0fa
DianLiR <2214058099@qq.com>
2020-12-25 16:39:20 +0800

    2020年12月25日 16点39分
    修改

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -21,11 +21,6 @@
           class="right_list"
           finished-text="没有更多了"
         >
-          <!--          <van-cell-->
-          <!--            v-for="item in ranking.books"-->
-          <!--            :key="item"-->
-          <!--            :title="item.title"-->
-          <!--          />-->
           <book-card
             v-for="(item, index) in ranking.books"
             :key="index"
@@ -38,107 +33,80 @@
 </template>
 
 <script>
-import {mapState} from 'vuex'
-import BookCard from '../components/BookCard'
+import { mapState } from "vuex";
+import BookCard from "../components/BookCard";
 
 export default {
-  name: 'Ranking',
+  name: "Ranking",
   components: { BookCard },
   data() {
     return {
-      type: 'epub',
-      active: 'epub',
+      type: "epub",
       ranking: null,
       activeKey: 0,
       loading: true,
       finished: true,
       tabListDate: [
         {
-          title: '出版',
-          name: 'epub',
+          title: "出版",
+          name: "epub",
         },
         {
-          title: '男生',
-          name: 'male',
+          title: "男生",
+          name: "male",
         },
         {
-          title: '女生',
-          name: 'female',
+          title: "女生",
+          name: "female",
         },
       ],
-    }
+    };
   },
   computed: {
-    ...mapState(['rankCategory']),
+    ...mapState(["rankCategory"]),
   },
   created() {
-    this.getList()
+    this.getList();
   },
   watch: {
     type() {
-      this.activeKey = 0
-      this.getList()
+      this.activeKey = 0;
+      this.getList();
     },
     activeKey() {
-      this.getList()
+      this.getList();
     },
   },
   methods: {
     onClickVanTab(name) {
-      this.$router.push('/ranking?type=' + name)
-    },
-    // getRanking(id) {
-    //   this.axios({
-    //     url: '/rank/' + id,
-    //   }).then((res) => {
-    //     this.ranking = res.data.ranking
-    //   })
-    // },
-    getList() {
-      // this.getRanking(this.rankCategory[this.type][this.activeKey]._id)
-      let id = this.rankCategory[this.type][this.activeKey]._id
-      this.$toast.loading({
-        message: '加载中...',
-        forbidClick: true,
-        // overlay: true,
-      })
-      this.axios({
-        url: '/rank/' + id,
-      }).then((res) => {
-        this.$toast.clear()
-        this.ranking = res.data.ranking
-      })
+      this.$router.push("/ranking?type=" + name);
     },
 
-    // onLoad() {
-    //   // 异步更新数据
-    //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-    //   setTimeout(() => {
-    //     for (let i = 0; i < 10; i++) {
-    //       // this.list.push(this.list.length + 1)
-    //       this.list.push(this.ranking.books[i])
-    //     }
-    //
-    //     // 加载状态结束
-    //     this.loading = false
-    //
-    //     // 数据全部加载完成
-    //     if (this.list.length >= 40) {
-    //       this.finished = true
-    //     }
-    //   }, 1000)
-    // },
+    getList() {
+      let id = this.rankCategory[this.type][this.activeKey]._id;
+      this.$toast.loading({
+        message: "加载中...",
+        forbidClick: true,
+        // overlay: true,
+      });
+      this.axios({
+        url: "/rank/" + id,
+      }).then((res) => {
+        this.$toast.clear();
+        this.ranking = res.data.ranking;
+      });
+    },
   },
   beforeRouteEnter(t, f, n) {
     n((vm) => {
-      vm.type = t.query.type || 'epub'
-    })
+      vm.type = t.query.type || "epub";
+    });
   },
   beforeRouteUpdate(t, f, n) {
-    this.type = t.query.type || 'epub'
-    n()
+    this.type = t.query.type || "epub";
+    n();
   },
-}
+};
 </script>
 
 <style lang="less" scoped>
